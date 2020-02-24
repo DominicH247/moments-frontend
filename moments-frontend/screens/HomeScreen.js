bee95732dcfcfcbe874a9e1f7f1504d22b05ae08
Alexander Trout <alexandertrout@A1.local>
2020-02-24 17:28:51 +0000

    basic functionality running

--- moments-frontend/screens/HomeScreen.js
+++ moments-frontend/screens/HomeScreen.js
@@ -1,20 +1,116 @@
 import * as React from "react";
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import * as WebBrowser from "expo-web-browser";
-import { MonoText } from "../components/StyledText";
+import { Component } from "react";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import axios from "axios";
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container}></ScrollView>
-    </View>
-  );
+class HomeScreen extends Component {
+  state = {
+    image: null,
+    uploaded: true
+  };
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+      const { status2 } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status2 !== "granted") {
+        alert("Sorry, we need camera permissions to make this work!");
+      }
+    }
+  };
+
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+    if (!result.cancelled) {
+      this.setState({ image: result });
+    }
+  };
+
+  takePicture = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+    if (!result.cancelled) {
+      this.setState({ image: result });
+    }
+  };
+
+  uploadImage = event => {
+    // let file = Platform.OS === "android" ? this.state.image.uri : this.state.image.uri.replace("file://", "");
+    let file = this.state.image.uri.replace("file://", "");
+    const data = new FormData();
+    data.append("profileImage", { uri: file, name: "image.jpeg" });
+    axios
+      .post("https://calm-scrubland-54250.herokuapp.com/api/images/", data, {
+        headers: {
+          accept: "application/json",
+          "Accept-Language": "en-US,en;q=0.8",
+          "Content-Type": `multipart/form-data; boundary=${data._boundary}`
+        }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          axios.patch(
+            `https://k8445cuwvd.execute-api.eu-west-2.amazonaws.com/latest/api/photos/crookydan`,
+            { photos: response.data.location }
+          );
+        }
+        this.setState({ image: null });
+      })
+      .catch(error => {
+        console.log("--------> Big Ol' Error -------->", error);
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View>
+            <Text style={styles.text}>Please select an image</Text>
+            {this.state.image && (
+              <Image style={styles.photoContainer} source={{ uri: this.state.image.uri }}></Image>
+            )}
+          </View>
+          <View style={styles.buttonContainer}>
+            <>
+              <Button color="white" onPress={this.pickImage} title="Camera Roll"></Button>
+              <Button color="white" onPress={this.takePicture} title="Take Photo"></Button>
+            </>
+            {this.state.image && (
+              <Button
+                color="white"
+                onPress={this.uploadImage}
+                title="Upload Image to S3 Bucket"
+              ></Button>
+            )}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
-HomeScreen.navigationOptions = {
-  header: null
-};
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -22,6 +118,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#266A2F"
   },
   contentContainer: {
-    paddingTop: 30
+    paddingTop: 30,
+    flex: 1,
+    alignItems: "center"
+  },
+  photoContainer: {
+    flex: 0,
+    alignItems: "center",
+    width: 400,
+    height: 400
+  },
+  text: {
+    color: "white",
+    fontSize: 30,
+    padding: 20
   }
 });
--- moments-frontend/screens/LinksScreen.js
+++ moments-frontend/screens/LinksScreen.js
@@ -1,17 +1,57 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Component } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
+import axios from "axios";
 
-export default function LinksScreen() {
-  return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    ></ScrollView>
-  );
+class LinksScreen extends Component {
+  state = {
+    photos: [],
+    updated: false
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://k8445cuwvd.execute-api.eu-west-2.amazonaws.com/latest/api/photos/crookydan")
+      .then(response => {
+        this.setState({ photos: response.data.data.Item.picURL });
+      });
+  }
+
+  updatePhotos = () => {
+    axios
+      .get("https://k8445cuwvd.execute-api.eu-west-2.amazonaws.com/latest/api/photos/crookydan")
+      .then(response => {
+        console.log(response.data.data.Item.picURL);
+        this.setState({ photos: response.data.data.Item.picURL });
+      });
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View>
+          <Text style={styles.text}>Your Images</Text>
+          <Button color="white" onPress={this.updatePhotos} title="Update Photos"></Button>
+          <View style={styles.photoContainer}>
+            {this.state.photos.reverse().map(url => {
+              console.log(url);
+              return (
+                <View>
+                  <Image style={styles.onePhoto} source={{ url }}></Image>
+                </View>
+              );
+            })}
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
+
+export default LinksScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,6 +59,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#266A2F"
   },
   contentContainer: {
-    paddingTop: 15
+    paddingTop: 15,
+    alignItems: "center"
+  },
+  photoContainer: {
+    flex: 0,
+    width: 400,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly"
+  },
+  onePhoto: {
+    marginTop: 25,
+    flex: 0,
+    height: 150,
+    width: 150
+  },
+  text: {
+    color: "white",
+    fontSize: 30,
+    padding: 20
   }
 });
