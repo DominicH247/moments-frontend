f4a1e4370cc0a86776a3188a9dc87babb2c7fc7a
keirbetts <keirjbetts@gmail.com>
2020-02-28 09:16:59 +0000

    tested sending photo to db

--- moments-frontend/screens/HomeScreen.js
+++ moments-frontend/screens/HomeScreen.js
@@ -92,7 +92,7 @@ class HomeScreen extends Component {
       const data = new FormData();
       data.append("profileImage", { uri: file, name: "image.jpeg" });
       axios
-        .post("https://calm-scrubland-54250.herokuapp.com/api/images/", data, {
+        .post("https://moments-s3.herokuapp.com/api/upload", data, {
           headers: {
             accept: "application/json",
             "Accept-Language": "en-US,en;q=0.8",
@@ -101,9 +101,9 @@ class HomeScreen extends Component {
         })
         .then(response => {
           if (response.status === 200) {
-            axios.patch(
-              `https://k8445cuwvd.execute-api.eu-west-2.amazonaws.com/latest/api/photos/crookydan`,
-              { photos: response.data.location }
+            axios.post(
+              `https://0cu7huuz9g.execute-api.eu-west-2.amazonaws.com/latest/api/upload/`,
+              { imageLocation: response.data.location, usr: "crookydan" }
             );
           }
           this.setState({ image: [], visible: false });
